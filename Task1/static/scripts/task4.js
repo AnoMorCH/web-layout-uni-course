a6050852f5d6bf186d80028d6411387c80064c2f
Anton Morozov <anomorch@gmail.com>
2022-11-23 15:46:45 +0300

    make task41

--- Task1/static/scripts/task4.js
+++ Task1/static/scripts/task4.js
@@ -1,3 +1,27 @@
+function getRandomNaturalNumberByInterval(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomUniqueIndex(list, maxIndexValue) {
+    let index = getRandomNaturalNumberByInterval(0, maxIndexValue);
+
+    while (list.includes(index)) {
+        index = getRandomNaturalNumberByInterval(0, maxIndexValue);
+    }
+
+    return index;
+}
+
+function updateCellsByEvenness(cell1, cell2, usedRowsAmount) {
+    if (usedRowsAmount % 2 == 0) {
+        cell1.classList.add(colorClass1);
+        cell2.classList.add(colorClass1);
+    } else {
+        cell1.classList.add(colorClass2);
+        cell2.classList.add(colorClass2);
+    }
+}
+
 const latingAdage = [
     '"Consuetudo est altera natura"',
     '"Nota bene"',
@@ -11,3 +35,43 @@ const russianAdageAnalog = [
     '"Беда не приходит одна"',
     '"Через тернии к звёздам"'
 ]
+
+const adageAmount = latingAdage.length;
+const everyAdageIsUsedMessage = 'Фразы закончились';
+
+const colorClass1 = '_class1';
+const colorClass2 = '_class2';
+
+// Task 4.1.
+const tbody41 = document.getElementById('tbody-task-4-1');
+const btnTask41 = document.getElementById('btn-task-4-1');
+
+let usedRowsAmount = 0;
+let usedRowsIndexes = [];
+
+btnTask41.addEventListener('click', function () {
+    if (usedRowsAmount < adageAmount) {
+        let adageIndex = getRandomUniqueIndex(usedRowsIndexes, adageAmount);
+        usedRowsAmount += 1;
+        usedRowsIndexes.push(adageIndex);
+
+        const row = document.createElement('tr');
+        const cell1 = document.createElement('td');
+        const cell2 = document.createElement('td');
+
+        const customCellClassName = '_table-cell';
+        cell1.classList.add(customCellClassName);
+        cell2.classList.add(customCellClassName);
+
+        updateCellsByEvenness(cell1, cell2, usedRowsAmount);
+
+        cell1.innerHTML = latingAdage[adageIndex];
+        cell2.innerHTML = russianAdageAnalog[adageIndex];
+
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        tbody41.appendChild(row);
+    } else {
+        alert(everyAdageIsUsedMessage);
+    }
+});
