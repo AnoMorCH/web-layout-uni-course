46251abfa885beb3eaecf08790daa07387f9e02f
Anton Morozov <anomorch@gmail.com>
2022-11-23 14:21:22 +0300

    create a pattern for task4

---
+++ Task1/static/scripts/task4.js
@@ -0,0 +1,13 @@
+const latingAdage = [
+    '"Consuetudo est altera natura"',
+    '"Nota bene"',
+    '"Nulla calamitas sola"',
+    '"Per aspera ad astra"'
+]
+
+const russianAdageAnalog = [
+    '"Привычка - вторая натура"',
+    '"Заметьте хорошо!"',
+    '"Беда не приходит одна"',
+    '"Через тернии к звёздам"'
+]
