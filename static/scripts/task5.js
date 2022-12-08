395a81396e0ce0e377a383a6814143f6f46e5de5
Anton Morozov <anomorch@gmail.com>
2022-12-08 15:31:25 +0300

    create check for the figure

--- static/scripts/task5.js
+++ static/scripts/task5.js
@@ -3,6 +3,9 @@ const columnsNumber = 8;
 const tilesAmount = 4;
 const checkedClassName = '_checked';
 
+const tilesLocation = '/static/images/dachshund/'
+const tilesExtention = '.png';
+
 let currentTile;
 let otherTile;
 
@@ -13,9 +16,6 @@ const puzzleTiles = document.getElementById('puzzle-wrapper');
 
 window.onload = () => {
     initialize8x3Board();
-
-    const tilesLocation = '/static/images/dachshund/'
-    const tilesExtention = '.png';
 
     const tilesId = getFilledTilesId();
     mixFigureTiles(tilesId);
@@ -36,8 +36,9 @@ window.onload = () => {
     turnCheckedTilesBtn.addEventListener('click', () => {
         checkedTiles.forEach((tile) => {
             let currentRotation = tile.style.rotate;
-            currentRotation = getCurrentRotationNumPlus90Deg(currentRotation);
+            currentRotation = getRotateStyleAsNum(currentRotation) + 90;
             tile.style.rotate = `${currentRotation}deg`;
+            checkIfPlayerSucceed();
         });
     });
 }
@@ -46,19 +47,19 @@ function initialize8x3Board() {
     for (let rowId = 0; rowId < rowsNumber; rowId++) {
         for (let columnId = 0; columnId < columnsNumber; columnId++) {
             let tile = document.createElement('img');
-            tile.src = '/static/images/blank.jpg';
+            tile.src = '/static/images/blank.png';
 
             makeElementDraggable(tile);
+            changeTileDataGivenItsProperty(tile);
 
             puzzleTiles.append(tile);
         }
     }
 }
 
-function getCurrentRotationNumPlus90Deg(currentRotation) {
+function getRotateStyleAsNum(currentRotation) {
     currentRotation = currentRotation.replace('deg', '');
     currentRotation = Number(currentRotation);
-    currentRotation += 90;
     return currentRotation;
 }
 
@@ -107,6 +108,66 @@ function makeElementDraggable(tile) {
     tile.addEventListener('dragend', dragEnd);
 }
 
+function getTileNumber(tile) {
+    tile = tile.src.split('/');
+    tile = tile[tile.length - 1];
+    tile = tile.replace(tilesExtention, ' ');
+    tile = Number(tile);
+    return tile;
+}
+
+function isTileRotateCorrect(tileRotate) {
+    return tileRotate % 360 == 0;
+}
+
+function isNextTileIdCorrect(nextTileId, index) {
+    return nextTileId == index + 1;
+}
+
+function isTileStartOfFigure(tileId) {
+    return tileId == 1;
+}
+
+function checkIfPlayerSucceed() {
+    const tilesArray = Array.from(puzzleTiles.children);
+
+    const rowsArray = [
+        tilesArray.slice(columnsNumber * 0, columnsNumber * 1),
+        tilesArray.slice(columnsNumber * 1, columnsNumber * 2),
+        tilesArray.slice(columnsNumber * 2, columnsNumber * 3)
+    ]
+
+    rowsArray.forEach((row) => {
+        for (let column = 0; column < columnsNumber; column++) {
+            const tile = row[column];
+            const tileId = getTileNumber(tile);
+
+            if (isTileStartOfFigure(tileId)) {
+                const tileRotate = getRotateStyleAsNum(tile.style.rotate);
+
+                if (!isTileRotateCorrect(tileRotate)) {
+                    return;
+                }
+
+                for (let i = 1; i < tilesAmount; i++) {
+                    const nextTile = row[column + i];
+                    const nextTileId = getTileNumber(nextTile);
+                    const nextTileRotate = getRotateStyleAsNum(nextTile.style.rotate);
+
+                    if (
+                        !isNextTileIdCorrect(nextTileId, i)
+                        || !isTileRotateCorrect(nextTileRotate)
+                    ) {
+                        return;
+                    }
+                }
+
+                alert('Фигура собрана правильно!');
+            }
+        }
+    });
+}
+
 // Set up drag functions.
 
 function dragStart() {
@@ -147,4 +208,6 @@ function dragEnd() {
     otherTile.style.rotate = currentImgRotate;
     otherTile.classList.add(checkedClassName);
     checkedTiles.push(otherTile);
+
+    checkIfPlayerSucceed();
 }
